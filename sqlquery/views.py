240bceec365767e5ed1cdf86a093a174d801c265
Aryaman A <aryaman.awasthi79@gmail.com>
2023-10-28 00:07:18 +0530

    feat: view creation

--- sqlquery/views.py
+++ sqlquery/views.py
@@ -1,3 +1,11 @@
 from django.shortcuts import render
+from models import Product
+import json
+from django.http import JsonResponse
+from django.core.serializers import serialize
 
-# Create your views here.
+def home(request):
+      qs = Product.objects.all()
+      serialized_data = serialize("json", qs)
+      serialized_data = json.loads(serialized_data)
+      return JsonResponse(serialized_data, status=200)
