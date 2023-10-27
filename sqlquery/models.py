6c09fa7208ee0d738ad3fdf8d30ff4903cb3bb81
Aryaman A <aryaman.awasthi79@gmail.com>
2023-10-27 23:25:24 +0530

    feat: model creation

--- sqlquery/models.py
+++ sqlquery/models.py
@@ -1,3 +1,11 @@
 from django.db import models
 
 # Create your models here.
+
+class Product(models.Model):
+      name = models.CharField(max_length=100)
+      slug = models.SlugField(max_length=100)
+      is_digital = models.BooleanField(default=False)
+      
+      def __str__(self):
+            return self.name
