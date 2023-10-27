2b44789882cbf2ebb6ba8d8c7169dc009aac04f2
Aryaman A <aryaman.awasthi79@gmail.com>
2023-10-28 01:36:11 +0530

    feat: created querycheck middleware

--- sqlquery/views.py
+++ sqlquery/views.py
@@ -3,22 +3,9 @@ from .models import Product
 import json
 from django.http import JsonResponse
 from django.core.serializers import serialize
-from django.db import connection
-from pygments import highlight
-from pygments.formatters import TerminalFormatter
-from pygments.lexers import SqlLexer
-from sqlparse import format
 
 def home(request):
       qs = Product.objects.all()
       serialized_data = serialize("json", qs)
-
-      # capturing and printing sql queries
-      queries = list(connection.queries)
-      for qs in queries:
-            sqlformatted = format(str(qs["sql"]), reindent=True)
-            print(highlight(sqlformatted, SqlLexer(), TerminalFormatter()))
-
-
       serialized_data = json.loads(serialized_data)
       return JsonResponse(serialized_data, safe=False, status=200)
