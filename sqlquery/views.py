51cfc4ed9ff6e049521855b5426e3f80d906d3e2
Aryaman A <aryaman.awasthi79@gmail.com>
2023-10-28 00:18:37 +0530

    fix: general fixes

--- sqlquery/views.py
+++ sqlquery/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from models import Product
+from .models import Product
 import json
 from django.http import JsonResponse
 from django.core.serializers import serialize
@@ -8,4 +8,4 @@ def home(request):
       qs = Product.objects.all()
       serialized_data = serialize("json", qs)
       serialized_data = json.loads(serialized_data)
-      return JsonResponse(serialized_data, status=200)
+      return JsonResponse(serialized_data, safe=False, status=200)
