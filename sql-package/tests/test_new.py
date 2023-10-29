6ab2dc29eb07c03f7b29b2d12731829607400356
Aryaman A <aryaman.awasthi79@gmail.com>
2023-10-30 01:11:08 +0530

    test: testing standard output

--- sql-package/tests/test_new.py
+++ sql-package/tests/test_new.py
@@ -1,13 +1,20 @@
 from test_app.models import Product
 import pytest
 from django.urls import reverse
+import sys
 
 pytestmark = pytest.mark.django_db
 
 @pytest.mark.urls('test_app.urls')
-def test_demo(client, settings):
+def test_demo(client, settings, capsys):
       settings.DEBUG = True
       url = reverse("test_endpoint")
       response = client.get(url)
+
+      captured = capsys.readouterr()
+      sys.stdout.write(captured.out)
+
+      assert "1 Toal Queries" in captured.out
+
       assert response.status_code == 200
 
